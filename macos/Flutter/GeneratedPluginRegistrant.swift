4f235558d68ae0f9f43a0abae24ebf700563a021
Thorsten Lorenz <thlorenz@gmx.de>
2021-02-14 13:13:36 -0500

    flutter: add macos support to app

---
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,12 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import workout_ffi
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  WorkoutFfiPlugin.register(with: registry.registrar(forPlugin: "WorkoutFfiPlugin"))
+}
